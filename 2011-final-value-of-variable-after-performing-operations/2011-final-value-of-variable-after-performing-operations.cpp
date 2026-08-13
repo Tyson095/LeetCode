0d39da5157ba929300f2175096c18fd9e386a4d7
Harshit Tyagi <htyagi2007@gmail.com>
2026-08-13 23:08:32 +0530

    Time: 0 ms (100%), Space: 17.8 MB (10.28%) - LeetHub

---
+++ 2011-final-value-of-variable-after-performing-operations/2011-final-value-of-variable-after-performing-operations.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    int finalValueAfterOperations(vector<string>& operations) {
+        int ans = 0 ;
+
+        for(auto i : operations) {
+            if(i == "--X" || i == "X--") {
+                ans-- ;
+            }else if(i == "X++" || i == "++X") {
+                ans++ ;
+            }
+        }
+
+        return ans ;
+    }
+};

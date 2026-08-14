edf6f1d5feceef417a0085d8d605ee7b81cf3586
Harshit Tyagi <htyagi2007@gmail.com>
2026-08-14 21:36:15 +0530

    Time: 1 ms (29.82%), Space: 44.7 MB (50.88%) - LeetHub

---
+++ 3861-minimum-capacity-box/3861-minimum-capacity-box.java
@@ -0,0 +1,17 @@
+class Solution {
+    public int minimumIndex(int[] capacity, int itemSize) {
+        int ans = -1 ;
+
+        for(int i = 0 ; i < capacity.length ; i++) {
+            if(capacity[i] >= itemSize) {
+                if(ans == -1) {
+                    ans = i ;
+                }else if(capacity[ans] > capacity[i]) {
+                    ans = i ;
+                }
+            }
+        }
+
+        return ans ;
+    }
+}

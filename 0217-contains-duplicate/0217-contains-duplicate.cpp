12bf8ca1bd1ca74afe1eb1fd28435c92db332ca4
Harshit Tyagi <htyagi2007@gmail.com>
2026-09-01 08:39:05 +0530

    Time: 71 ms (60.92%), Space: 111.4 MB (28.15%) - LeetHub

---
+++ 0217-contains-duplicate/0217-contains-duplicate.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    bool containsDuplicate(vector<int>& nums) {
+        unordered_map<int, int> m ;
+
+        for(int i : nums)  {
+            m[i]++ ;
+
+            if(m[i] > 1) {
+                return true ;
+            }
+        }
+
+        return false ;
+    }
+};

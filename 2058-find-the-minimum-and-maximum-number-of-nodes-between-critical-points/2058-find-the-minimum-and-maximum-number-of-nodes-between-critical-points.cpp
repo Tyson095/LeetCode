84a4906a53fa19dfa8b31674a10b33efe8a77cbd
Harshit Tyagi <htyagi2007@gmail.com>
2026-09-01 18:22:22 +0530

    Time: 3 ms (67.31%), Space: 124.4 MB (96.02%) - LeetHub

---
+++ 2058-find-the-minimum-and-maximum-number-of-nodes-between-critical-points/2058-find-the-minimum-and-maximum-number-of-nodes-between-critical-points.cpp
@@ -0,0 +1,37 @@
+class Solution {
+public:
+    vector<int> nodesBetweenCriticalPoints(ListNode* head) {
+        vector<int> ans = {-1, -1} ;
+
+        int idx = 1, firstCrit = -1, prevCrit = -1;
+
+        ListNode* curr = head->next ;
+        int prev = head->val ;
+
+        while(curr->next != NULL) {
+            int val = curr->val ;
+            int nxt = curr->next->val ;
+
+            if( (val > nxt && val > prev) || (val < nxt && val < prev) ) {
+                if(prevCrit == -1) {
+                    firstCrit = idx ;
+                    prevCrit = idx ;
+                }else {
+                    ans[0] = (ans[0] == 0 || ans[0] == -1) ? idx-prevCrit : min(ans[0], idx-prevCrit) ;
+                    prevCrit = idx ;
+                }
+                
+            }
+
+            curr = curr->next ;
+            prev = val ;
+            idx++ ;
+        }
+
+        if(ans[0] != -1) {
+            ans[1] = prevCrit - firstCrit ;
+        }
+
+        return ans ;
+    }
+};

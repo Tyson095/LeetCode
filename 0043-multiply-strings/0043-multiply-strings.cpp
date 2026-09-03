b35aa105ecda051d654e5ffcddfc3e55851fdad3
Harshit Tyagi <htyagi2007@gmail.com>
2026-09-03 22:41:17 +0530

    Time: 0 ms (100%), Space: 9.6 MB (49.41%) - LeetHub

--- 0043-multiply-strings/0043-multiply-strings.cpp
+++ 0043-multiply-strings/0043-multiply-strings.cpp
@@ -1,82 +1,32 @@
 class Solution {
 public:
-    string add(string nums1, string nums2) {
-        string ans ;
-
-        int i = nums1.size()-1, j = nums2.size()-1 ;
-        int carry = 0 ;
-
-        while(i >= 0 && j >= 0) {
-
-            int a = nums1[i--] - '0' ;
-            int b = nums2[j--] - '0' ;
-
-            int x = (a + b + carry) % 10 ;
-            carry = (a + b + carry) / 10 ;
-
-            ans.push_back('0' + x) ;
-        }
-
-        while(i >= 0) {
-            int a = nums1[i--] - '0' ;
-            int x = (a + carry) % 10 ;
-            carry = (a + carry) / 10 ;
-            ans.push_back('0' + x) ;
-        }
-
-        while(j >= 0) {
-            int a = nums2[j--] - '0' ;
-            int x = (a + carry) % 10 ;
-            carry = (a + carry) / 10 ;
-            ans.push_back('0' + x) ;
-        }
-        
-        if(carry != 0) {
-            ans.push_back(carry + '0') ;
-        }
-
-        reverse(ans.begin(), ans.end()) ;
-        return ans ;
-    }
-
-
-    string multiply(string nums1, string nums2) {
-        if (nums1 == "0" || nums2 == "0") {
+    string multiply(string num1, string num2) {
+        if(num1[0] == '0' || num2[0] == '0'){
             return "0";
         }
-        string ans = "0" ;
 
-        int i = nums1.size()-1, j = nums2.size()-1 ;
+        vector<int> num(num1.size() + num2.size(), 0) ;
 
-        while(j >= 0) {
-            string curr ;
-            int carry = 0 ;
-
-            int temp = j ;
-            while(temp < nums2.size()-1) {
-                curr.push_back('0') ;
-                temp++ ;
+        for(int i = num1.size()-1 ; i >= 0 ; i--) {
+            for(int j = num2.size()-1 ; j>=0 ; j--) {
+                num[i+j+1] += (num1[i] - '0') * (num2[j] - '0') ;
+                num[i+j] += num[i+j+1] / 10 ;
+                
+                num[i+j+1] %= 10 ;  
             }
-
-            temp = i ;
-            while(temp >= 0) {
-                int a = nums1[temp--] - '0' ;
-                int b = nums2[j] - '0' ;
-
-                int x = ((a*b) + carry) % 10 ;
-                carry = ((a*b) + carry) / 10 ;
-
-                curr.push_back('0' + x) ;
-            }
-            if(carry != 0) {
-                curr.push_back('0' + carry) ;
-            }
-
-            reverse(curr.begin(), curr.end()) ;
-            ans = add(ans, curr) ;
-            j-- ;
+        }
+        
+        // now remove the leading 0's
+        int i = 0 ;
+        while(num[i] == 0){
+            i++ ;
         }
 
+        string ans = "" ;
+        while(i < num.size()){
+            ans.push_back(num[i] + '0') ;
+            i++ ;
+        }
         return ans ;
     }
 };
